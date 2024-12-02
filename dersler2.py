0809cebeebbb726324432d40133d63e7206e751d
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 16:51:56 +0300

    another very common mistake in the code

--- dersler2.py
+++ dersler2.py
@@ -3,7 +3,7 @@ fibonacci_list = list()
 fibonacci_list.append(1)
 fibonacci_sayisi = 0
 while len(fibonacci_list) != 100:
-    sayi + sayi = sayi2
+    sayi2 = sayi + sayi
     sayi3 = sayi2 + sayi
     fibonacci_list.append(sayi3)
 print(fibonacci_list)
