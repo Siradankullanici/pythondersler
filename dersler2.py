5640cd6e6d40ff9bbadefc8df6eb44086db248f3
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 16:50:44 +0300

    optimize

--- dersler2.py
+++ dersler2.py
@@ -2,10 +2,8 @@ sayi = 1
 fibonacci_list = list()
 fibonacci_list.append(1)
 fibonacci_sayisi = 0
-while True:
+while len(fibonacci_list) != 100:
     sayi + sayi = sayi2
     sayi3 = sayi2 + sayi
     fibonacci_list.append(sayi3)
-    if len(fibonacci_list) == 100:
-        break
 print(fibonacci_list)
