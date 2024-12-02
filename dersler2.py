f6d0e3225a8c7bc2377473a27ba3664bdb201ae8
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 13:12:05 +0300

    Update dersler2.py

--- dersler2.py
+++ dersler2.py
@@ -1,6 +1,14 @@
 # 3 basamaklı sayıların kaç tanaesi rakamların küplerine toplamına eşittir? **3
 liste = []
+liste2 = []
 ucbasamak = range(100, 1000)
 
 for sayi in ucbasamak:
     kupu = sayi ** 3
+    birlesme = str(kupu)
+    for rakam in birlesme:
+    	liste2.append(int(rakam))
+    	toplam = sum(liste2)
+    	if sayi == toplam:
+    	  liste.append(sayi)
+print(len(liste))
