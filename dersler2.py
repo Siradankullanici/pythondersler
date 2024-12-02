da2ad86c3f8d8afc42a448b6288e738ff40bb458
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 13:56:23 +0300

    asama bitiyor

--- dersler2.py
+++ dersler2.py
@@ -1,14 +1,18 @@
-# 3 basamaklı sayıların kaç tanaesi rakamların küplerine toplamına eşittir? **3
+# 3 basamaklı sayıların kaç tanaesi rakamların küplerine toplamına eşittir? **3 153 gibi = 1 küp 5 küp 3 küp farklı optimizasyonlar yapılabilir
 liste = []
 liste2 = []
+liste3 = []
+
 ucbasamak = range(100, 1000)
 
 for sayi in ucbasamak:
-    kupu = sayi ** 3
-    birlesme = str(kupu)
+    birlesme = str(sayi)
     for rakam in birlesme:
-    	liste2.append(int(rakam))
-    	toplam = sum(liste2)
-    	if sayi == toplam:
-    	  liste.append(sayi)
+      liste2.append(int(rakam))
+	  for eleman in liste2:
+        elemankup = eleman ** 3
+    	liste3.append(elemankup)
+        toplam = sum(liste3)
+    	    if sayi == toplam:
+    	      liste.append(sayi)
 print(len(liste))
