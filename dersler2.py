0853adb19aa9084757c9db5d902eb9bc9c4902e3
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 12:13:17 +0300

    old code

--- dersler2.py
+++ dersler2.py
@@ -1,8 +1,8 @@
 # 3 basamaklı sayıların kaç tanaesi rakamların küplerine toplamına eşittir? **3
 liste = []
-for sayi in range(100, 1000):
+ucbasamak = range(100, 1000)
+for sayi in ucbasamak:
     str_sayi = str(sayi)
     for harf in str_sayi:
       int(harf)
       print(harf)
-
