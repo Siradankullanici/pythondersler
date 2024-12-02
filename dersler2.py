8281a5fd922e2aff1c8522cd8434e14cfa0814cf
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 16:42:23 +0300

    Daha yaygın hata

--- dersler2.py
+++ dersler2.py
@@ -3,8 +3,9 @@ fibonacci_list = list()
 fibonacci_list.append(1)
 fibonacci_sayisi = 0
 while True:
-    sayi += sayi
-    fibonacci_list.append(sayi)
+    sayi + sayi = sayi2
+    sayi3 = sayi2 + sayi
+    fibonacci_list.append(sayi3)
     if len(fibonacci_list) == 100:
         break
 print(fibonacci_list)
