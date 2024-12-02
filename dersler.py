7c2aeb3550701716577f4d886b7b8e87c0e9f425
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-12-02 12:05:53 +0300

    Deneme 1

--- dersler.py
+++ dersler.py
@@ -755,21 +755,6 @@ sayi = int(input("Bir sayi giriniz: "))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 """ 
 liste1 = ["a", "b", "c"]
 liste2 = [1, 2, 3]
@@ -788,3 +773,31 @@ liste = [1, 2, 3, 4, 5, 6, 7, 8, 9] """
 
 for harf, adet in sozluk.items():
     print(harf, adet) """
+
+prime_list = list()
+
+prime_list.append(2)
+
+sayi = 3
+
+while True:
+    prime = True
+    for i in range(2, int(sayi ** 0.5) + 1): #0.5 kuvveti yani tam uyumlu 25/5 5 olur bu yüzden bundan büyükse yani +1 o zaman devam ediyoruz
+        if sayi %i == 0:
+            prime = False
+            break
+        if prime:
+            prime_list.append(sayi)
+            if len(prime_list) == 10000:
+                break
+    sayi += 1        
+
+liste2 = [] # boş liste
+
+for prime in prime_list:
+    strprime = str(prime)
+    if strprime.startswith("3") and strprime.endswith("7"):
+      liste2.append(prime)
+
+print(liste2)
+print(len(liste2))
--- dersler2.py
+++ dersler2.py
@@ -1,27 +1,8 @@
-prime_list = list()
-
-prime_list.append(2)
-
-sayi = 3
-
-while True:
-    prime = True
-    for i in range(2, int(sayi ** 0.5) + 1): #0.5 kuvveti yani tam uyumlu 25/5 5 olur bu yüzden bundan büyükse yani +1 o zaman devam ediyoruz
-        if sayi %i == 0:
-            prime = False
-            break
-        if prime:
-            prime_list.append(sayi)
-            if len(prime_list) == 10000:
-                break
-    sayi += 1        
-
-liste2 = [] # boş liste
-
-for prime in prime_list:
-    strprime = str(prime)
-    if strprime.startswith("3") and strprime.endswith("7"):
-      liste2.append(prime)
-
-print(liste2)
-print(len(liste2))
+# 3 basamaklı sayıların kaç tanaesi rakamların küplerine toplamına eşittir? **3
+liste = []
+ucbasamak = range(100, 1000)
+for sayi in ucbasamak:
+    str_sayi = str(sayi)
+    for harf in str_sayi:
+      int(harf)
+      print(harf)
